3f5cad07d44404e728752af2c1d79db34d16216e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-27 00:04:56 -0500

    Update shadowsocks-manager.sh

--- shadowsocks-manager.sh
+++ shadowsocks-manager.sh
@@ -452,7 +452,7 @@ root hard nofile 51200" >>${SYSTEM_LIMITS}
             tar xvzf "${V2RAY_PLUGIN_PATH}"
             rm -f "${V2RAY_PLUGIN_PATH}"
             find "${SHADOWSOCKS_COMMON_PATH}" -name "v2ray*" -exec mv {} "${SHADOWSOCKS_COMMON_PATH}"/v2ray-plugin \;
-            if { [ "${MODE_CHOICE}" == "tcp_only" ] && [ "${SERVER_PORT}" == "80" ]; then
+            if { [ "${MODE_CHOICE}" == "tcp_only" ] && [ "${SERVER_PORT}" == "80" ]; }; then
                 PLUGIN_CHOICE="v2ray-plugin"
                 PLUGIN_OPTS="server"
             elif { [ "${MODE_CHOICE}" == "tcp_only" ] && [ "${SERVER_PORT}" == "443" ]; }; then
