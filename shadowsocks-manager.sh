f9ac53e1a48414308aa0545a5f065d963d62b263
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-18 12:54:50 -0400

    Update shadowsocks-manager.sh

--- shadowsocks-manager.sh
+++ shadowsocks-manager.sh
@@ -364,7 +364,7 @@ if [ ! -f "${SHADOWSOCKS_CONFIG_PATH}" ]; then
     function install-shadowsocks-server() {
         if [ ! -x "$(command -v rustup)" ]; then
             curl https://sh.rustup.rs -sSf | sh -s -- -y
-            source $HOME/.cargo/env
+            source ${HOME}/.cargo/env
             rustup default nightly
             if [ ! -x "$(command -v ssserver)" ]; then
                 cargo install shadowsocks-rust
